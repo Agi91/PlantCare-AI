5d8d89ad82bd338a528f43d3b9a5a5817e46fcc1
Agi91 <umashankarsahu6691@gmail.com>
2026-03-11 11:38:23 +0530

    changes for requirement.txt

--- app/app.py
+++ app/app.py
@@ -66,4 +66,8 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Render environment variable se port uthata hai
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+    
